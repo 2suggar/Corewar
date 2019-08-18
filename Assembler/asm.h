f22331b996b0464a10a8cf3bc6b753ed2d2dc624
Luwin Cutjack <lcutjack@oa-n4.21-school.ru>
2019-08-18 13:45:50 +0300

    lets start

--- Assembler/asm.h
+++ Assembler/asm.h
@@ -6,7 +6,7 @@
 /*   By: lcutjack <lcutjack@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 17:41:30 by lcutjack          #+#    #+#             */
-/*   Updated: 2019/08/17 22:34:37 by lcutjack         ###   ########.fr       */
+/*   Updated: 2019/08/18 13:31:37 by lcutjack         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@
 # include "op.h"
 # include <stdio.h>
 
-typedef struct s_out {
-    char    *header;
-    int     code_size;
-    char    *comm;
-    char    *code;
-}               t_out;
+typedef struct	s_out {
+    char	header[4];
+    char	name[PROG_NAME_LENGTH];
+    char	name_null[4];
+    char	code_size[4];
+    char	comm[COMMENT_LENGTH];
+    char	com_null[4];
+    char	*code;
+}				t_out;
 
 void    say_error(char *fname, char id);
 void	cook_raw(int fd, int new);
--- Assembler/cooking.c
+++ Assembler/cooking.c
@@ -6,73 +6,88 @@
 /*   By: lcutjack <lcutjack@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/17 14:12:42 by lcutjack          #+#    #+#             */
-/*   Updated: 2019/08/17 22:37:33 by lcutjack         ###   ########.fr       */
+/*   Updated: 2019/08/18 13:45:42 by lcutjack         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-static void write_magic(int new, t_out *out)
+static void write_magic(t_out *out)
 {
     char    *lol;
     int     value;
     int     i;
-    char    magic[4];
 
     i = 4;
     value = COREWAR_EXEC_MAGIC;
     lol = (char*)&value;
     while (--i >= 0)
-        magic[i] = *(lol + i);
-    out->header = magic;
+        out->head[i] = *(lol + i);
 }
 
-static void read_name(int fd, int new)
+static void read_name(int fd, t_out *out)
 {
-    char    name[PROG_NAME_LENGTH + 4];
     char    *line;
     char    *start;
 
-    get_next_line(fd, &line);
-	while (!*line || *line == COMMENT_CHAR)
-		get_next_line(fd, &line);
-    if (ft_strncmp(NAME_CMD_STRING, line, ft_strlen(NAME_CMD_STRING)))
+	if (ft_strncmp(NAME_CMD_STRING, line, ft_strlen(NAME_CMD_STRING)))
     {
-        ft_bzero(name, PROG_NAME_LENGTH);
+
         start = ft_strchr(line, '"');
-        ft_strncpy(name, start + 1, ft_strchr(start + 1, '"') - start - 1);
+        ft_strncpy(out->name, start + 1, ft_strchr(start + 1, '"') - start - 1);
     }
-    write(new, name, PROG_NAME_LENGTH);
 }
 
-static void read_comment(int fd, int new)
+static void read_comment(int fd, t_out *out)
 {
-    char comment[COMMENT_LENGTH + 4];
-    char *line;
-    char *start;
+    char    *line;
+    char    *start;
+    char    *end;
 
-    get_next_line(fd, &line);
-	while (!*line || *line == COMMENT_CHAR)
-		get_next_line(fd, &line);
-    if (ft_strncmp(COMMENT_CMD_STRING, line, ft_strlen(COMMENT_CMD_STRING)))
-    {
-        ft_bzero(comment, COMMENT_LENGTH);
-        start = ft_strchr(line, '"');
-        ft_strncpy(comment, start + 1, ft_strchr(start + 1, '"') - start - 1);
-    }
-    write(new, comment, COMMENT_LENGTH);
+	start = ft_strchr(line, '"');
+	if ((end = ft_strchr(start + 1, '"')))
+		ft_strncpy(out->comm, start + 1, end - start - 1);
+	else
+	{
+		ft_strcat(out->comm, start);
+       	while (get_next_line(fd, start) && !(end = ft_strchr(start, '"')))
+       	{
+			ft_strcat(out->comm, start);
+			free(start);
+       	}
+		ft_strncat(out->comm, start, end - start - 1);
+	}
 }
 
+static void	read_n_c(int fd, t_out *out)
+{
+	int		i;
+	char	*line;
 
+	i = 2;
+	while (i--)
+	{
+		get_next_line(fd, &line);
+		while (!*line || *line == COMMENT_CHAR)
+		{
+			free(line);
+			get_next_line(fd, &line);
+		}
+		if (ft_strncmp(COMMENT_CMD_STRING, line, ft_strlen(COMMENT_CMD_STRING)))
+			read_comment(fd, out);
+		else if (ft_strncmp(NAME_CMD_STRING, line, ft_strlen(NAME_CMD_STRING)))
+			read_name(fd, out);
+	}
+}	
 
 void    cook_raw(int fd, int new)
 {
-    t_out *out;
+    t_out	*out;
 
 	if (!(out = ft_memalloc(sizeof(t_out))))
 		return ;
-    write_magic(out);
-    read_name(fd, out);
-    read_comment(fd, out);
+	ft_bzero(out, sizeof(t_out));
+	write_magic(out);
+	read_n_c(fd, out);
     // read_code();
 }
